6b3237e34d5d207a5c35f03c0975aecf4efd5611
AO Industries, Inc <support@aoindustries.com>
2020-02-29 16:37:52 -0600

    Switched API from getTempFileContext(…) to getInstance(…).
    Old methods remain, deprecated.
    
    Also switched implementation to use inner Initializer class, which is
    consistent with other projects we maintain.

--- src/main/java/com/semanticcms/core/controller/CapturePage.java
+++ src/main/java/com/semanticcms/core/controller/CapturePage.java
@@ -118,7 +118,7 @@ public class CapturePage {
 			request,
 			response,
 			new HttpServletSubRequestWrapper(request),
-			new HttpServletSubResponseWrapper(response, ServletTempFileContext.getTempFileContext(request)),
+			new HttpServletSubResponseWrapper(response, ServletTempFileContext.getInstance(request)),
 			pageReferrer,
 			level,
 			cache
@@ -280,7 +280,7 @@ public class CapturePage {
 				&& ConcurrencyCoordinator.useConcurrentSubrequests(request)
 			) {
 				// Concurrent implementation
-				final TempFileContext tempFileContext = ServletTempFileContext.getTempFileContext(request);
+				final TempFileContext tempFileContext = ServletTempFileContext.getInstance(request);
 				final HttpServletRequest threadSafeReq = new UnmodifiableCopyHttpServletRequest(request);
 				final HttpServletResponse threadSafeResp = new UnmodifiableCopyHttpServletResponse(response);
 				// Create the tasks
@@ -505,7 +505,7 @@ public class CapturePage {
 				edges,
 				edgeFilter,
 				null,
-				ServletTempFileContext.getTempFileContext(request),
+				ServletTempFileContext.getInstance(request),
 				cache,
 				new HashSet<>()
 			);
@@ -547,7 +547,7 @@ public class CapturePage {
 			preferredConcurrency = executors.getPreferredConcurrency();
 			assert preferredConcurrency > 1 : "Single-CPU systems should never make it to this concurrent implementation";
 		}
-		final TempFileContext tempFileContext = ServletTempFileContext.getTempFileContext(request);
+		final TempFileContext tempFileContext = ServletTempFileContext.getInstance(request);
 
 		int maxSize = 0;
 
@@ -863,7 +863,7 @@ public class CapturePage {
 				edges,
 				edgeFilter,
 				postHandler,
-				ServletTempFileContext.getTempFileContext(request),
+				ServletTempFileContext.getInstance(request),
 				cache,
 				new HashSet<>()
 			);
