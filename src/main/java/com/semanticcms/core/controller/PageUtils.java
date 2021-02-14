7bddc814746701ad2c3ac430a8b52bb467d3ca6b
AO Industries, Inc <support@aoindustries.com>
2021-02-14 08:02:27 -0600

    Released ao-encoding-3.3.0

--- src/main/java/com/semanticcms/core/controller/PageUtils.java
+++ src/main/java/com/semanticcms/core/controller/PageUtils.java
@@ -1,6 +1,6 @@
 /*
  * semanticcms-core-controller - Serves SemanticCMS content from a Servlet environment.
- * Copyright (C) 2013, 2014, 2015, 2016, 2017, 2019, 2020  AO Industries, Inc.
+ * Copyright (C) 2013, 2014, 2015, 2016, 2017, 2019, 2020, 2021  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -23,7 +23,7 @@
 package com.semanticcms.core.controller;
 
 import com.aoindustries.collections.AoCollections;
-import com.aoindustries.encoding.Coercion;
+import com.aoindustries.lang.Coercion;
 import com.semanticcms.core.model.BookRef;
 import com.semanticcms.core.model.ChildRef;
 import com.semanticcms.core.model.Element;
